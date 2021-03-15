4b92fd9267d4bc2f4c0b5d8af0233b5779a30379
Heroponne <55738457+Heroponne@users.noreply.github.com>
2021-03-15 14:46:25 +0100

    added Track entity

---
+++ API-blind-test/src/Entity/Track.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrackRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrackRepository::class)
+ */
+class Track
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artist;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $trackUrl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getArtist(): ?string
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(string $artist): self
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    public function getTrackUrl(): ?string
+    {
+        return $this->trackUrl;
+    }
+
+    public function setTrackUrl(string $trackUrl): self
+    {
+        $this->trackUrl = $trackUrl;
+
+        return $this;
+    }
+}
