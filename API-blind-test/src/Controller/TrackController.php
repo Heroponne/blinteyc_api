0d337d1efe47f17895e8cd6b746eca107c59458d
Heroponne <55738457+Heroponne@users.noreply.github.com>
2021-03-17 14:05:34 +0100

    edited showTrackAction

--- API-blind-test/src/Controller/TrackController.php
+++ API-blind-test/src/Controller/TrackController.php
@@ -2,20 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Track;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrackController extends AbstractController
 {
     /**
-     * @Route("/track", name="track")
+     * @Route("/tracks/{id}", name="track_show", methods={"GET"})
+     * @param SerializerInterface $serializer
+     * @param Track $track
+     * @return Response
      */
-    public function index(): Response
+
+    public function showTrackAction(SerializerInterface $serializer, Track $track): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TrackController.php',
-        ]);
+        //on serialize, avec l'objet à sérialiser et le format souhaité
+        $data = $serializer->serialize($track, 'json');
+
+        //on construit la réponse avec les data sérialisées
+
+        return new Response($data, Response::HTTP_OK, [
+            'Content-Type' => 'text/plain',
+            'Access-Control-Allow-Origin' => '*']);
+
     }
 }
