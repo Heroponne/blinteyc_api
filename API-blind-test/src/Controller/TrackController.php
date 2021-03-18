e2fe9a32cef4d0f6921f38947f26af7694932f9f
Heroponne <55738457+Heroponne@users.noreply.github.com>
2021-03-18 16:26:32 +0100

    reorganized showTrackAction return

--- API-blind-test/src/Controller/TrackController.php
+++ API-blind-test/src/Controller/TrackController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Track;
+use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,12 +21,12 @@ class TrackController extends AbstractController
     public function showTrackAction(SerializerInterface $serializer, Track $track): Response
     {
         //on serialize, avec l'objet à sérialiser et le format souhaité
-        $data = $serializer->serialize($track, 'json');
+        $data = $serializer->serialize($track, 'json', SerializationContext::create()->setGroups(array('game')));
 
         //on construit la réponse avec les data sérialisées
 
         return new Response($data, Response::HTTP_OK, [
-            'Content-Type' => 'text/plain',
+            'Content-Type' => 'application/json',
             'Access-Control-Allow-Origin' => '*']);
 
     }
